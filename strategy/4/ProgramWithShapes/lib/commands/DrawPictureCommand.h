8ac6eccfbbc8d7ebc01088bb020713804fade328
raketa0 <rychkovalex04@gmail.com>
2025-09-26 00:54:11 +0300

    Сделал команду рисования Picture

---
+++ strategy/4/ProgramWithShapes/lib/commands/DrawPictureCommand.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include"ICommand.h"
+#include <string>
+#include "../shapes/CPicture.h"
+
+namespace command
+{
+    class DrawPictureCommand : public ICommand 
+    {
+    public:
+        DrawPictureCommand(shapes::CPicture& picture,
+            gfx::ICanvas& canvas)
+            : m_picture(picture), m_canvas(canvas) 
+        {
+        }
+
+        void Execute() override 
+        {
+            m_picture.DrawPicture(m_canvas);
+        }
+
+    private:
+        shapes::CPicture& m_picture;
+        gfx::ICanvas& m_canvas;
+    };
+}
--- strategy/4/ProgramWithShapes/lib/commands/DrawShapeCommand.h
+++ strategy/4/ProgramWithShapes/lib/commands/DrawShapeCommand.h
@@ -1,1 +1,31 @@
 #pragma once
+#include"ICommand.h"
+#include <string>
+#include "../shapes/CPicture.h"
+
+namespace command
+{
+    class DrawShapeCommand : public ICommand 
+    {
+    public:
+        DrawShapeCommand(shapes::CPicture& picture,
+            const std::string& id,
+            gfx::ICanvas& canvas)
+            : m_picture(picture), m_id(id), m_canvas(canvas) {}
+
+        void Execute() override 
+        {
+            auto shape = m_picture.GetShape(m_id);
+            if (shape) 
+            {
+                shape->DrawShape(m_canvas);
+            }
+        }
+
+    private:
+        shapes::CPicture& m_picture;
+        std::string m_id;
+        gfx::ICanvas& m_canvas;
+    };
+
+}
