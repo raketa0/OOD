65d2a87288bb3325be213832d4f889d0087f9811
raketa0 <rychkovalex04@gmail.com>
2025-09-26 00:49:39 +0300

    Сделал команды изменения типа фигуры и рисование фигуры

---
+++ strategy/4/ProgramWithShapes/lib/commands/ChangeShapeCommand.h
@@ -0,0 +1,32 @@
+#pragma once
+#include"ICommand.h"
+#include <string>
+#include "../shapes/CPicture.h"
+
+namespace command
+{
+    class ChangeShapeCommand : public ICommand
+    {
+    public:
+        ChangeShapeCommand(shapes::CPicture& picture,
+            const std::string& id,
+            std::unique_ptr<shapes::IShapeStrategy> && shapeStrategy)
+            : m_picture(picture),
+            m_id(id),
+            m_shapeStrategy(std::move(shapeStrategy)) {}
+
+        void Execute() override 
+        {
+            auto shape = m_picture.GetShape(m_id);
+            if (shape) 
+            {
+                shape->ChangeShape(std::move(m_shapeStrategy));
+            }
+        }
+
+    private:
+        shapes::CPicture& m_picture;
+        std::string m_id;
+        std::unique_ptr<shapes::IShapeStrategy> && m_shapeStrategy;
+    };
+}
