66124fd2c88929a5d33e07f46aee7edfc0f8cfcd
raketa0 <rychkovalex04@gmail.com>
2025-10-24 09:35:35 +0300

    Update ICanvas.h

--- programm_shapes/program_shapes/lib/canvas/ICanvas.h
+++ programm_shapes/program_shapes/lib/canvas/ICanvas.h
@@ -5,8 +5,8 @@
 class ICanvas
 {
 public:
-    virtual void drawCircle(Point centerCircle, int radius) = 0;
-    virtual void drawRectangle(Point leftTopCorner, int width, int height) = 0;
+    virtual void drawCircle(Point centerCircle, int radius) = 0; // с большой буквы
+    virtual void drawRectangle(Point leftTopCorner, int width, int height) = 0; 
     virtual void drawTriangle(Point firstPoint, Point secondPoint, Point thirdPoint) = 0;
 };
 
